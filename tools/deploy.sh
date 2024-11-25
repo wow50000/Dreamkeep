56422c568e34b54f59a760e251b2797c37d2bf87
Noelle Lavenza <jadebishop.2562@gmail.com>
2024-11-25 12:57:42 -0500

    Fix CI not starting

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -23,13 +23,21 @@ if [ -d ".git" ]; then
   cp -r .git/logs/* $1/.git/logs/
 fi
 
-cp tgstation.dmb tgstation.rsc $1/
+cp roguetown.dmb roguetown.rsc $1/
 cp -r _maps/* $1/_maps/
+cp -r icons/*_title.dmi $1/icons/
 cp -r icons/effects/* $1/icons/effects/
 cp -r icons/mob/clothing/* $1/icons/mob/clothing/
 cp -r icons/mob/inhands/* $1/icons/mob/inhands/
-cp -r icons/mob/simple/* $1/icons/mob/simple/
+cp -r icons/roguetown/* $1/icons/roguetown/
 cp -r icons/obj/* $1/icons/obj/
+cp -r modular_hearthstone/icons/obj/* $1/modular_hearthstone/icons/obj/
+cp -r modular_helmsguard/licensed-eaglephntm/* $1/modular_helmsguard/licensed-eaglephntm/
+cp -r modular_helmsguard/licensed-feralope/* $1/modular_helmsguard/licensed-feralope/
+cp -r modular_stonehedge/licensed-eaglephntm/* $1/modular_stonehedge/licensed-eaglephntm/
+cp -r modular_stonehedge/licensed-feralope/* $1/modular_stonehedge/licensed-feralope/
+cp -r modular_stonehedge/licensed-death-rattler/* $1/modular_stonehedge/licensed-feralope/
+cp -r modular_stonehedge/icons/* $1/modular_stonehedge/icons/
 cp -r strings/* $1/strings/
 
 #remove .dm files from _maps
