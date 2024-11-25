Darkrp-community/OpenKeep

Add and fix unit tests

code/modules/unit_tests/spawn_humans.dm
@@ -1,7 +1,7 @@
 /datum/unit_test/spawn_humans/Run()
-    var/locs = block(run_loc_bottom_left, run_loc_top_right)
+	var/locs = block(run_loc_bottom_left, run_loc_top_right)
 
-    for(var/I in 1 to 5)
-        new /mob/living/carbon/human(pick(locs))
+	for(var/I in 1 to 5)
+		allocate(/mob/living/carbon/human, pick(locs))
 
-    sleep(50)
+	sleep(50)

wow50000/Dreamkeep

code/modules/unit_tests/spawn_humans.dm
@@ -1,7 +1,7 @@
 /datum/unit_test/spawn_humans/Run()
-    var/locs = block(run_loc_floor_bottom_left, run_loc_floor_top_right)
+	var/locs = block(run_loc_floor_bottom_left, run_loc_floor_top_right)
 
-    for(var/I in 1 to 5)
-        new /mob/living/carbon/human(pick(locs))
+	for(var/I in 1 to 5)
+		allocate(/mob/living/carbon/human, pick(locs))
 
-    sleep(50)
+	sleep(50)